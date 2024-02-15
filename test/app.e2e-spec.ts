becccc41fba9c466c84a3342f2075403df3b0e5c
zied-snoussi <ziedsnoussi.tn@gmail.com>
2024-02-15 11:24:47 +0100

    update comments

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -11,16 +11,16 @@ import { OrderModule } from '../src/order/order.module';
 import { RatingModule } from '../src/rating/rating.module';
 import { AuthModule } from '../src/auth/auth.module';
 import { LoginDtoStub, UpdateUserDtoStub, UserDtoStub, UserDtoStub2 } from '../src/test/stubs/user.dto.stub';
-import { UserService } from '../src/user/user.service'; // Import UserService
+import { UserService } from '../src/user/user.service';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
-  let accessToken: string;
-  let refreshToken: string;
-  let userIdAdmin: string;
-  let userId: string;
-  let userrole: string = "admin";
-  let fakeUserId: string = "60f3e3e3e3e3e3e3e3e3e3e3";
+  let app: INestApplication; // Declare app variable
+  let accessToken: string; // Declare accessToken variable
+  let refreshToken: string; // Declare refreshToken variable
+  let userIdAdmin: string; // Declare userIdAdmin variable
+  let userId: string; // Declare userId variable
+  let userrole: string = "admin"; // Declare userrole variable
+  let fakeUserId: string = "60f3e3e3e3e3e3e3e3e3e3e3"; // Declare fakeUserId variable
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -49,23 +49,25 @@ describe('AppController (e2e)', () => {
       ],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    app = moduleFixture.createNestApplication(); // Create a new Nest application
+    await app.init(); // Initialize the application
   });
 
   afterEach(async () => {
-    await app.close();
+    await app.close(); // Close the application after each test
   });
 
+  // this test for register should be the first test to run
   it('/auth/register (POST)', async () => {
     const response = await request(app.getHttpServer())
       .post('/auth/register')
       .send(UserDtoStub)
       .expect(201); // Corrected expected status code to 201 for Created
   
-    expect(response.body).toHaveProperty('username');
+    expect(response.body).toHaveProperty('username'); // Check if response body has username property
   });
 
+// this test for login should be the first test to run
   it('/auth/login (POST)', async () => {
     const response = await request(app.getHttpServer())
       .post('/auth/login')
@@ -77,55 +79,49 @@ describe('AppController (e2e)', () => {
     userId = response.body.user._doc._id;
     userIdAdmin = response.body.user._doc._id;
     userrole = response.body.user._doc.role;
-    expect(response.body).toHaveProperty('backendTokens.accessToken');
-    expect(response.body).toHaveProperty('backendTokens.refreshToken');
+    expect(response.body).toHaveProperty('backendTokens.accessToken'); // Check if response body has accessToken property
+    expect(response.body).toHaveProperty('backendTokens.refreshToken'); // Check if response body has refreshToken property
   });
 
+  // this test for get should be the last test to run
   it('/users (GET)', async () => {
     const response = await request(app.getHttpServer())
       .get('/users')
       .set('userrole', userrole)
       .set('Authorization', `Bearer ${accessToken}`)
-      .expect(200); // Corrected expected status code to 401 for Unauthorized
+      .expect(200); // Assuming it returns 200 for successful request
 
-    expect(response.body).toBeInstanceOf(Array);
+    expect(response.body).toBeInstanceOf(Array); // Check if response body is an instance of Array
   });
 
-  // it('/auth/refresh (POST)', async () => {
-  //   const response = await request(app.getHttpServer())
-  //     .post('/auth/refresh')
-  //     .send({ refreshToken })
-  //     .set('Authorization', `Bearer ${accessToken}`)
-  //     .expect(200);
-
-  //   expect(response.body).toHaveProperty('accessToken');
-  // });
-
+ // this test for post should be the last test to run
   it('/users (POST)', async () => {
     const response = await request(app.getHttpServer())
       .post('/users')
       .send(UserDtoStub2)
-      .set('userrole', userrole)
-      .set('Authorization', `Bearer ${accessToken}`)
+      .set('userrole', userrole) // Set the userrole header
+      .set('Authorization', `Bearer ${accessToken}`) // Set the Authorization header
       .expect(201); // Assuming it returns 201 for successful creation
     userId = response.body._id;
-    expect(response.body).toHaveProperty('username');
-    expect(response.body).toHaveProperty('email');
-    expect(response.body).toHaveProperty('role');
+    expect(response.body).toHaveProperty('username'); // Check if response body has username property
+    expect(response.body).toHaveProperty('email'); // Check if response body has email property
+    expect(response.body).toHaveProperty('role'); // Check if response body has role property
   });
 
+  // this test for get should be the last test to run
   it('/users/{id} (GET)', async () => {
     const response = await request(app.getHttpServer())
       .get(`/users/${userId}`)
       .set('userrole', userrole)
       .set('Authorization', `Bearer ${accessToken}`)
-      .expect(200);
+      .expect(200); // Assuming it returns 200 for successful request
 
-    expect(response.body).toHaveProperty('username');
-    expect(response.body).toHaveProperty('email');
-    expect(response.body).toHaveProperty('role');
+    expect(response.body).toHaveProperty('username'); // Check if response body has username property
+    expect(response.body).toHaveProperty('email');  // Check if response body has email property
+    expect(response.body).toHaveProperty('role'); // Check if response body has role property
   });
 
+  // it('/users/{id} (GET) should return 401 if the user is not authorized', async () => {
   it('/users/{id} (PATCH)', async () => {
     const response = await request(app.getHttpServer())
       .patch(`/users/${userId}`)
@@ -134,31 +130,34 @@ describe('AppController (e2e)', () => {
       .set('Authorization', `Bearer ${accessToken}`)
       .expect(200); // Assuming it returns 200 for successful update
 
-    expect(response.body).toHaveProperty('email');
-    expect(response.body).toHaveProperty('role');
+    expect(response.body).toHaveProperty('email'); // Check if response body has email property
+    expect(response.body).toHaveProperty('role'); // Check if response body has role property
   });
 
+  // this test for delete should be the last test to run
   it('/users/{id} (DELETE)', async () => {
     const response = await request(app.getHttpServer())
       .delete(`/users/${userId}`)
       .set('userrole', userrole)
-      .set('Authorization', `Bearer ${accessToken}`)
+      .set('Authorization', `Bearer ${accessToken}`) // Set the Authorization header
       .expect(200); // Assuming it returns 200 for successful deletion
-    expect(response.body).toEqual({});
+    expect(response.body).toEqual({}); // Check if response body is an empty object
   });
 
+  // this test for post should be the last test to run
   it('/users (POST) should return 400 if the user already exists', async () => {
     const response = await request(app.getHttpServer())
       .post('/users')
       .send(UserDtoStub)
       .set('userrole', userrole)
       .set('Authorization', `Bearer ${accessToken}`)
-      .expect(422); // Assuming it returns 400 for duplicate user
+      .expect(422); // Assuming it returns 422 for user already exists
 
-    expect(response.body).toHaveProperty('message');
-    expect(response.body.message).toEqual("Email is already taken");
+    expect(response.body).toHaveProperty('message'); // Check if response body has message property
+    expect(response.body.message).toEqual("Email is already taken"); // Check if response body has message property
   });
 
+  // this test for get should be the last test to run
   it('/users/{id} (GET) should return 404 if the user is not found', async () => {
     const response = await request(app.getHttpServer())
       .get(`/users/${fakeUserId}`)
@@ -167,9 +166,10 @@ describe('AppController (e2e)', () => {
       .expect(404); // Assuming it returns 404 for user not found
 
     expect(response.body).toHaveProperty('message');
-    expect(response.body.message).toEqual("No user found");
+    expect(response.body.message).toEqual("No user found"); // Check if response body has message property
   });
 
+  // this test for update should be the last test to run
   it('/users/{id} (PATCH) should return 404 if the user is not found', async () => {
     const response = await request(app.getHttpServer())
       .patch(`/users/${fakeUserId}`)
@@ -178,29 +178,31 @@ describe('AppController (e2e)', () => {
       .set('Authorization', `Bearer ${accessToken}`)
       .expect(404); // Assuming it returns 404 for user not found
 
-    expect(response.body).toHaveProperty('message');
-    expect(response.body.message).toEqual("User not found");
+    expect(response.body).toHaveProperty('message'); // Check if response body has message property
+    expect(response.body.message).toEqual("User not found"); // Check if response body has message property
   });
 
+  // this test for delete should be the last test to run
   it('/users/{id} (DELETE) should return 404 if the user is not found', async () => {
     const response = await request(app.getHttpServer())
       .delete(`/users/${fakeUserId}`)
       .set('userrole', userrole)
-      .set('Authorization', `Bearer ${accessToken}`)
+      .set('Authorization', `Bearer ${accessToken}`) // Set the Authorization header
       .expect(404); // Assuming it returns 404 for user not found
 
-    expect(response.body).toHaveProperty('message');
-    expect(response.body.message).toEqual("User not found");
+    expect(response.body).toHaveProperty('message'); // Check if response body has message property
+    expect(response.body.message).toEqual("User not found"); // Check if response body has message property
   });
 
+  // this test for delete should be the last test to run
   it('/users/{id} (DELETE) should delete the admin user', async () => {
     const response = await request(app.getHttpServer())
-      .delete(`/users/${userIdAdmin}`)
+      .delete(`/users/${userIdAdmin}`) // Delete the admin user
       .set('userrole', userrole)
-      .set('Authorization', `Bearer ${accessToken}`)
+      .set('Authorization', `Bearer ${accessToken}`) // Set the Authorization header
       .expect(200); // Assuming it returns 200 for successful deletion
 
-    expect(response.body).toEqual({});
+    expect(response.body).toEqual({}); // Check if response body is an empty object
   });
 
 });
